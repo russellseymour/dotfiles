45cd6c4a377b98661a25553bf092e66f6d1d9d01
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-02-04 14:18:29 +0000

    Added tmux configuration

--- .chezmoitemplates/config/setup.bash
+++ .chezmoitemplates/config/setup.bash
@@ -15,3 +15,8 @@ sudo apt install -y tmux \
 
 # Install oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# Install Tmux plugin manager if it does not already exist
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
