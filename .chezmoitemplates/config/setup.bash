32362f49a99b0c9cd3023a9cca1efedbf3102615
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-02-04 13:54:44 +0000

    Corrected various issues Adding packages for Ubuntu
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- .chezmoitemplates/config/setup.bash
+++ .chezmoitemplates/config/setup.bash
@@ -2,3 +2,13 @@
 
 echo "Installing apt packages, press Ctrl+C to cancel, or enter password."
 
+# Configure additional repositories
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+
+# Update apt after installing new repositories
+sudo apt-update
+
+# Install required packages
+sudo apt install -y tmux \
+    neovim
+
