973dd60687c1a3b281f3097c80798e25cfed95c9
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2026-09-15 16:54:24 +0100

    Updated to handle Git signing properly in WSL

--- .chezmoitemplates/config/setup.bash
+++ .chezmoitemplates/config/setup.bash
@@ -25,3 +25,19 @@ curl -s https://ohmyposh.dev/install.sh | bash -s
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
+# Determine if running in WSL, if so then configure accordingly
+if grep -qi microsoft /proc/version 2>/dev/null || [ -n "${WSL_DISTRO_NAME}" ]; then
+    
+    # Add symlinks to allow the GPG agent to work correctly in WSL
+    sudo ln -s /mnt/c/Program\ Files/GnuPG/bin/gpg.exe $HOME/.local/bin/gpg
+    sudo ln -s /mnt/c/Windows/System32/OpenSSH/ssh.exe $HOME/.local/bin/ssh
+    sudo ln -s /mnt/c/Windows/System32/OpenSSH/ssh-add.exe $HOME/.local/bin/ssh-add
+    sudo ln -s /mnt/c/Windows/System32/OpenSSH/scp.exe $HOME/.local/bin/scp
+
+    # Configure Git
+    git config --global user.email russell.seymour@turtlesystems.co.uk
+    git config --global user.name "Russell Seymour"
+    git config --global user.signingkey A6E9C075A5A9348B
+    git config --global commit.gpgsign true
+fi
